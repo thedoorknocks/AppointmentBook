c95037f0b28d7831829c004ce8f85870d2ed2923
thedoorknocks <nathanl60@nycstudents.net>
2025-04-10 22:50:38 +0000

    v2

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -1,34 +1,54 @@
-public class AppointmentBook{
+public class AppointmentBook {
+
     private boolean[][] schedule;
 
-    public AppointmentBook(boolean[][] schedule)
-    {
+    public AppointmentBook(boolean[][] schedule) {
         this.schedule = schedule;
     }
-    public boolean isMinuteFree(int period, int minute){
-        return schedule[period-1][minute];
+
+    private boolean isMinuteFree(int period, int minute) {
+        return schedule[period - 1][minute];
     }
-    public boolean makeAppointmentBook(int startperiod, int endperiod, int duration){
-        return false;
-    }
-    public void printPeriod(int period){
-        for(int i =0; i<schedule[period-1].length; i++)
-        System.out.println(i +" " + schedule[period-1][i]);
-    }
-    public int findFreeBlock(int period, int duration){
-        int block=0;
-        for(int i =0; i<60;i++){
-            if(isMinuteFree(period,i)){
+
+    public int findFreeBlock(int period, int duration) {
+        int block = 0;
+        for (int minute = 0; minute < 60; minute++) {
+            if (isMinuteFree(period, minute)) {
                 block++;
-                if(block == duration){
-                    return i-duration+1;
+                if (block >= duration) {
+                    return minute - duration + 1;
                 }
-                else{
-                    block =0;
-                }
+            } else {
+                block = 0;
             }
         }
         return -1;
     }
 
+    private void reserveBlock(int period, int startMinute, int duration) {
+        for (int i = startMinute; i < startMinute + duration; i++) {
+            schedule[period - 1][i] = false;
+        }
+    }
+
+    public boolean makeAppointment(int duration) {
+        for (int period = 1; period <= 8; period++) {
+            int start = findFreeBlock(period, duration);
+            if (start != -1) {
+                reserveBlock(period, start, duration);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printPeriod(int period) {
+        System.out.print("Period " + period + ": ");
+        for (int i = 0; i < 60; i++) {
+            if (schedule[period - 1][i]) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,22 +1,32 @@
-public class Main{
-    public static void main(String args[]){
+public class Main {
+    public static void main(String[] args) {
         boolean[][] schedule = new boolean[8][60];
-        AppointmentBook a = new AppointmentBook(schedule);
-            
-        for(int i= 10; i<15; i++)schedule[1][i] = true;
-        a.printPeriod(2);
-        for(int i= 30; i<44; i++)schedule[1][i] = true;
-        a.printPeriod(2);
-        for(int i= 50; i<59; i++)schedule[1][i] = true;
-        a.printPeriod(2);
-        a.findFreeBlock(2,15);
-        schedule = new boolean[8][60];
-        for(int i= 25; i<30; i++)schedule[1][i] = true;
-        for(int i= 10; i<15; i++)schedule[2][i] = true;
-        for(int i= 25; i<30; i++)schedule[1][i] = true;
-        for(int i= 10; i<15; i++)schedule[2][i] = true;
-        for(int i= 25; i<30; i++)schedule[1][i] = true;
-        } 
+
+        for (int i = 0; i < schedule.length; i++) {
+            for (int j = 0; j < schedule[0].length; j++) {
+                schedule[i][j] = false;
+            }
+        }
+
+        for (int i = 10; i <= 14; i++) {
+            schedule[1][i] = true;
+        }
+
+        for (int i = 30; i <= 44; i++) {
+            schedule[1][i] = true;
+        }
+
+        for (int i = 50; i <= 59; i++) {
+            schedule[1][i] = true;
+        }
+
+        AppointmentBook book = new AppointmentBook(schedule);
+
+        book.printPeriod(2);
+
+        boolean result = book.makeAppointment(15);
+        System.out.println("Appointment success: " + result);
+
+        book.printPeriod(2);
+    }
 }
-
-
